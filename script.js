92ddc223ca43a393587cde0432357ec8cbd85a11
RealMinTech <adesinamariam7@gmail.com>
2025-05-27 11:31:32 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// This is function declaration
+function greet() {
+    // alert('Good morning Ade')
+    window.prompt("How old are you?")
+}
+
+// Variable 
+let school = "OIC"
+let name = "Mariam"
+let age = 12
+document.getElementById(
+  "bio"
+).innerHTML = `My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`;
+console.log(`My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`);
