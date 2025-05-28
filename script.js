c687990db80ac13b7995fd8f7087b9bd87c1a656
RealMinTech <adesinamariam7@gmail.com>
2025-05-28 11:01:03 +0100

    operators

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+// // This is function declaration
+// function greet() {
+//     // alert('Good morning Ade')
+//     window.prompt("How old are you?")
+// }
+
+// // Variable
+// let school = "OIC"
+// let name = "Mariam"
+// let age = 12
+// document.getElementById(
+//   "bio"
+// ).innerHTML = `My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`;
+// console.log(`My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`);
+
+// OPERATORS
+let a = 5;
+let b = 8;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a === b);
+console.log(a !== b);
+console.log(a <= b);
+let c = "Ade"
+let d = "Ola"
+console.log(c + d);
+a > 4 ? console.log("Good") : console.log("false");
+console.log(typeof c);
 
 
-// This is function declaration
-function greet() {
-    // alert('Good morning Ade')
-    window.prompt("How old are you?")
-}
 
-// Variable 
-let school = "OIC"
-let name = "Mariam"
-let age = 12
-document.getElementById(
-  "bio"
-).innerHTML = `My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`;
-console.log(`My name is ${name}. I am ${age} years old. I attended ${school} hub coding school.`);
+
+
+
